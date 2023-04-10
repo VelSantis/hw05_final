f55068eedd7c233bd42bf3e3f30f0e8f81984109
Андрей К <steel28_07@mail.ru>
2023-04-10 14:15:40 +0300

    11

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -20,6 +20,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+handler404 = "core.views.page_not_found"
+handler403 = 'core.views.permission_denied'
+handler500 = "core.views.server_error"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', include('about.urls', namespace='about')),
